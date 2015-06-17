4b01f6719389cbae6e0f8d656ba0854fb00db6a5
Paul Barry <mail@paulbarry.com>
2015-06-17 15:49:12 -0400

    basic app

---
+++ index.js
@@ -0,0 +1,18 @@
+// we need to require 'express' module
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+
+// A GET method route
+app.get('/', function (req, res) {
+  res.send('GET index page.');
+});
+
+// A POST method route
+app.post('/', function (req, res) {
+  res.send('POST index page.');
+});
+
+//boot up the server
+app.listen(port);
+console.log('The server is listening to ' + port);
